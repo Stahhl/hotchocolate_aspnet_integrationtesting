f15e33ddf8d270d602235424bcf0ad71f7048e74
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2024-04-20 19:42:24 +0200

    alternative method with strings for the Query, also use autogenerated input objects

--- Demo.Test/WebApplicationFactoryTests.cs
+++ Demo.Test/WebApplicationFactoryTests.cs
@@ -40,6 +40,40 @@ public class WebApplicationFactoryTests : IClassFixture<WebApplicationFactory<Pr
         Assert.True(booksElement.Any());
     }
 
+    [Fact]
+    public async Task GetAllBooksTestAlternative()
+    {
+        var request = new
+        {
+            Query =
+                """
+                query getAllBooksQuery {
+                  books {
+                    id
+                    book {
+                      title
+                      author {
+                        name
+                      }
+                    }
+                  }
+                }
+                """
+        };
+        _testOutputHelper.WriteLine($"request: {request}");
+
+        var client = _factory.CreateDefaultClient();
+        var response = await client.PostAsJsonAsync("/graphql", request);
+        var content = await response.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"response: {response.StatusCode}, body: {content}");
+        response.EnsureSuccessStatusCode();
+
+        using var jsonDocument = JsonDocument.Parse(content);
+        var booksElement = jsonDocument.RootElement.GetProperty("data").GetProperty("books").EnumerateArray();
+
+        Assert.True(booksElement.Any());
+    }
+
     [Theory]
     [InlineData(1, "C# in depth.")]
     public async Task GetBookByIdTest(int id, string expectedTitle)
@@ -47,6 +81,39 @@ public class WebApplicationFactoryTests : IClassFixture<WebApplicationFactory<Pr
         var graphQlDocument =
             _factory.Services.GetRequiredService<IDemoClient>().GetBookByIdQuery.Create(null).Document;
         var request = new { Query = Encoding.UTF8.GetString(graphQlDocument.Body), Variables = new { Id = id } };
+        _testOutputHelper.WriteLine($"request: {request}");
+
+        var client = _factory.CreateDefaultClient();
+        var response = await client.PostAsJsonAsync("/graphql", request);
+        var content = await response.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"response: {response.StatusCode}, body: {content}");
+        response.EnsureSuccessStatusCode();
+
+        using var jsonDocument = JsonDocument.Parse(content);
+        var titleElement = jsonDocument.RootElement.GetProperty("data").GetProperty("bookById").GetProperty("book")
+            .GetProperty("title").GetString();
+
+        Assert.Equal(expectedTitle, titleElement);
+    }
+
+    [Theory]
+    [InlineData(1, "C# in depth.")]
+    public async Task GetBookByIdTestAlternative(int id, string expectedTitle)
+    {
+        var request = new
+        {
+            Query =
+                """
+                query getBookByIdQuery($id: Int!) {
+                  bookById(id: $id) {
+                    book {
+                      title
+                    }
+                  }
+                }
+                """,
+            Variables = new { Id = id }
+        };
         _testOutputHelper.WriteLine($"request: {request}");
 
         var client = _factory.CreateDefaultClient();
@@ -72,24 +139,68 @@ public class WebApplicationFactoryTests : IClassFixture<WebApplicationFactory<Pr
             Query = Encoding.UTF8.GetString(graphQlDocument.Body),
             Variables = new
             {
-                Input = new
+                Input = new AddBookInput()
                 {
-                    Book = new { Title = "To foo or not to foo...", Author = new { Name = "Lord Test McTestish" } }
+                    Book = new BookInput()
+                    {
+                        Title = "To foo or not to foo...", Author = new AuthorInput() { Name = "Lord Test McTestish" }
+                    }
                 }
             }
         };
         _testOutputHelper.WriteLine($"request: {request}");
-        
+
         var client = _factory.CreateDefaultClient();
         var response = await client.PostAsJsonAsync("/graphql", request);
         var content = await response.Content.ReadAsStringAsync();
         _testOutputHelper.WriteLine($"response: {response.StatusCode}, body: {content}");
         response.EnsureSuccessStatusCode();
-        
+
         using var jsonDocument = JsonDocument.Parse(content);
         var idElement = jsonDocument.RootElement.GetProperty("data").GetProperty("addBook").GetProperty("bookEntity")
             .GetProperty("id").GetInt32();
-        
-        Assert.Equal(2, idElement);
+
+        Assert.True(idElement > 1);
+    }
+
+    [Fact]
+    public async Task AddBookTestAlternative()
+    {
+        var request = new
+        {
+            Query =
+                """
+                mutation addBookMutation($input: AddBookInput!) {
+                  addBook(input: $input) {
+                    bookEntity {
+                      id
+                    }
+                  }
+                }
+                """,
+            Variables = new
+            {
+                Input = new AddBookInput()
+                {
+                    Book = new BookInput()
+                    {
+                        Title = "To foo or not to foo...", Author = new AuthorInput() { Name = "Lord Test McTestish" }
+                    }
+                }
+            }
+        };
+        _testOutputHelper.WriteLine($"request: {request}");
+
+        var client = _factory.CreateDefaultClient();
+        var response = await client.PostAsJsonAsync("/graphql", request);
+        var content = await response.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine($"response: {response.StatusCode}, body: {content}");
+        response.EnsureSuccessStatusCode();
+
+        using var jsonDocument = JsonDocument.Parse(content);
+        var idElement = jsonDocument.RootElement.GetProperty("data").GetProperty("addBook").GetProperty("bookEntity")
+            .GetProperty("id").GetInt32();
+
+        Assert.True(idElement > 1);
     }
 }
